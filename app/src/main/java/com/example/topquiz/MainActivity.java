8536d5a670de211562fee09f11f8d76a835a69c7
Morgane Soula <morganesoula@MacBook-Air-de-Morgane.local>
2019-03-01 15:59:27 +0100

    Foundation of the app

--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_text);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_button);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mPlayButton.setEnabled(false);
+        mNameInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Bingo, it had been clicked
+            }
+        });
+
+
     }
 }
