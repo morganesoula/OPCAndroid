5f97e454ec16dd84906a8035ca492583482c3eb2
Morgane Soula <morganesoula@MacBook-Air-de-Morgane.local>
2019-03-01 16:52:08 +0100

    Modified order of variables in order to make it work

--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -17,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_text);
-        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
-        mPlayButton = (Button) findViewById(R.id.activity_main_play_button);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_text);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_button);
 
         mPlayButton.setEnabled(false);
         mNameInput.addTextChangedListener(new TextWatcher() {
